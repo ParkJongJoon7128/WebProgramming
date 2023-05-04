9e02715332246064d2a59bbb5474e202980301c5
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-05-04 12:07:28 +0900

    SNS 로그인 버튼 이미지 추가

--- src/main/java/com/example/webproject/controller/MainController.java
+++ src/main/java/com/example/webproject/controller/MainController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Main {
-    @GetMapping("/main")
+public class MainController {
+    @GetMapping("/")
     public String test(){
         return "Hello World! ";
     }
